1c89907fc8a85bf1c8d9fe62ec7f1019a59f4291
MiserableGamer <leomorse77@gmail.com>
2025-12-20 11:46:41 +0000

    Delete CommonLib directory

--- CommonLib/RegistryKeys.cs
+++
@@ -1,8 +0,0 @@
-namespace RomenH.Common
-{
-	public static class RegistryKeys
-	{
-		public const string LUTNotIncluded_DayLUT = "LUTNotIncluded.DayLUT";
-		public const string LUTNotIncluded_NightLUT = "LUTNotIncluded.NightLUT";
-	}
-}
