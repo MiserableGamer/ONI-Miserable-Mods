77b7646e4e131d1e994dbaf2dd10aa2132a3f787
MiserableGamer <miserablegamer@users.noreply.github.com>
2026-02-14 11:31:37 +0000

    Reset 43 file(s) outside ControlledAutomation dragged in by cherry-pick

---
+++ MorningExercise/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MorningExercise")]
+[assembly: AssemblyDescription("Adds a Morning Exercise schedule block for Duplicants to work out and gain an Athletics buff.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MiserableGamer")]
+[assembly: AssemblyProduct("MorningExercise")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8f3b4c1-2d5a-4f8e-9c1b-3a7d6e9f0123")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
