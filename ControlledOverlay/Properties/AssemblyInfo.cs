94c4360cce37249e0a3015565f13e3d945a25dcc
MiserableGamer <miserablegamer@users.noreply.github.com>
2026-02-14 11:31:53 +0000

    Reset 37 file(s) outside ControlledFiltering dragged in by cherry-pick

---
+++ ControlledOverlay/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ControlledOverlay")]
+[assembly: AssemblyDescription("Adds a text input field to the Temperature Overlay for exact temperature values")]
+[assembly: AssemblyProduct("ControlledOverlay")]
+[assembly: AssemblyCopyright("© MiserableGamer")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d5e6f7a8-b9c0-1234-5678-9abcdef01234")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+
